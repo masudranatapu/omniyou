77475339ba45161585435d5d647202fa9bbfd5b2
Md Maidul <mahi_rbs>
2022-10-23 11:32:32 +0600

    ..

--- app/Http/Middleware/Admin.php
+++ app/Http/Middleware/Admin.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class Admin
 {
@@ -14,14 +15,29 @@ class Admin
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
-    {
-        if(auth()->user()->role == 'admin'){
+    // public function handle($request, Closure $next)
+    // {
+    //     if(auth()->user()->role == 'admin'){
+    //         return $next($request);
+    //     }else{
+    //         return redirect()->back()->with('error', 'Sorry, You are not a admin !!');
+    //     }
+
+
+    // }
+
+    public function handle($request, Closure $next, String $role = null) {
+        if (!Auth::check()) // This isnt necessary, it should be part of your 'auth' middleware
+            return redirect('/');
+
+        $user = Auth::user();
+        if($user->role == $role)
             return $next($request);
-        }else{
-            return redirect()->back()->with('error', 'Sorry, You are not a admin !!');
-        }
+
+        return redirect('/');
     }
+
+
 }
 
 
